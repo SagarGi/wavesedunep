a5b47a5345442a97d8132adb10e0eb033e42fcfa
SagarGi <sagargurung1001@gmail.com>
2020-04-10 22:10:27 +0545

    carousel

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ class App extends Component
       <div className = "main">
 
         <div className = "wrapper">
-           <nav className = "navbar navbar-expand-lg  navbar-light  static-top">
+          
+           <nav className = "navbar navbar-expand-lg navbar-light  static-top">
 
             <div className = "container">
 
-              <a href = "" className = "navbar-brand"><img src = {require('./images/logowaves.png')} alt = "logo" className = "logo" /></a>
+              <a href = "" className = "navbar-brand"><img src = {require('./images/waveslogo.png')} alt = "logo" className = "logo" /></a>
                 {/* button when responsive */}
               <button className = "navbar-toggler" type = "button" data-toggle = "collapse" data-target = "#collapsenavbar">
                 <span className =  "navbar-toggler-icon"></span>
@@ -23,7 +24,7 @@ class App extends Component
 
               <div className = "collapse navbar-collapse text-center" id = "collapsenavbar">
 
-                <ul className = "navbar-nav ml-auto">
+                <ul className = "navbar-nav">
                     <li className = "nav-item">
                         <a href = "" className = "nav-link text-body"><p>Home</p></a>
                     </li>
@@ -46,8 +47,36 @@ class App extends Component
             </div>
 
            </nav>
+           
+           <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div className="carousel-inner" style = {{height: "60vh"}}>
+                <div className="carousel-item active">
+                  <img className="img-fluid d-block w-100" src = {require('./images/studyuk.jpg')}  alt="First slide" />
+                </div>
+                <div class="carousel-item">
+                  <img className="img-fluid d-block w-100" src = {require('./images/studyjapan.jpg')} alt="Second slide" />
+                </div>
+                <div className="carousel-item">
+                  <img className="img-fluid d-block w-100" src = {require('./images/studyaus.jpg')} alt="Third slide" />
+                </div>
+              </div>
+              <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+              </a>
+              <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+              </a>
+            </div>
+
         </div>
-        
+      
       </div>
     )
   }
