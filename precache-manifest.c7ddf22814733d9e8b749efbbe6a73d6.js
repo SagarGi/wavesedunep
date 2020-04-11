b8e256c9910da28f625c11e2ddf835fa8c3fa806
SagarGi <sagargurung1001@gmail.com>
2020-04-11 18:00:02 +0545

    Updates

---
+++ precache-manifest.c7ddf22814733d9e8b749efbbe6a73d6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3226e8d9f49957fee0bd897c8720452d",
+    "url": "/wavesedunep/index.html"
+  },
+  {
+    "revision": "e3f1bd5b6f332951b351",
+    "url": "/wavesedunep/static/css/main.572b5992.chunk.css"
+  },
+  {
+    "revision": "fdef4c10914f548ea786",
+    "url": "/wavesedunep/static/js/2.fe3d8a3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wavesedunep/static/js/2.fe3d8a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f1bd5b6f332951b351",
+    "url": "/wavesedunep/static/js/main.47b202ed.chunk.js"
+  },
+  {
+    "revision": "dad0c80efa9cc47245ad",
+    "url": "/wavesedunep/static/js/runtime-main.b80c17ec.js"
+  },
+  {
+    "revision": "19f6bcfe6055ea790a8a1f59e0e40f5d",
+    "url": "/wavesedunep/static/media/bijay.19f6bcfe.jpg"
+  },
+  {
+    "revision": "20b8a7a99e277dd5377e83719f4a38cc",
+    "url": "/wavesedunep/static/media/counselling.20b8a7a9.jpg"
+  },
+  {
+    "revision": "d613e669d0e4d0cc1793d31afa3fab43",
+    "url": "/wavesedunep/static/media/mansa.d613e669.jpg"
+  },
+  {
+    "revision": "d975923eac5e55ee5623efd8af3c80b2",
+    "url": "/wavesedunep/static/media/manzu.d975923e.jpg"
+  },
+  {
+    "revision": "1c80d508253a70b30445736a0422483d",
+    "url": "/wavesedunep/static/media/studyaus.1c80d508.jpg"
+  },
+  {
+    "revision": "ba398713c8b8729630ee9ce1f655ae04",
+    "url": "/wavesedunep/static/media/studyjapan.ba398713.jpg"
+  },
+  {
+    "revision": "af303fce75c57650d182afe80783e457",
+    "url": "/wavesedunep/static/media/studyuk.af303fce.jpg"
+  },
+  {
+    "revision": "625cfdb7c25e01bf000489cd1bdef7f0",
+    "url": "/wavesedunep/static/media/waveslogo.625cfdb7.png"
+  }
+]);
