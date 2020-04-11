65271543bbc21398d37b6337b6918ad4bdeb07ef
SagarGi <sagargurung1001@gmail.com>
2020-04-11 09:27:16 +0545

    about

--- src/App.js
+++ src/App.js
@@ -82,6 +82,32 @@ class App extends Component
             <div className = "container mt-4">
 
               <p className = "heading-para">Waves Education</p>
+              <hr></hr>
+
+              <p className = "about-para">Kangaroo Education Foundation (KEF) is an innovative and specialist educational<br></br>
+              consultancy service provider. In service of students since 2007, we specialize in<br></br>
+              educational counseling, recruitment and enrollment at overseas universities and <br></br>
+              colleges for more than 12 years. Adhering to the company’s policy of decentralization, <br></br>
+              to a total number of 8 offices in Australia and Nepal.
+              </p>
+
+
+            </div>
+
+            {/* our services section */}
+
+            <div className = "container">
+
+            <p className = "heading-para">Our Services</p>
+            <hr></hr>
+
+              <div className = "row">
+
+                <div className = "col-md-4"> 
+                    
+                </div>
+
+              </div>
 
             </div>
 
