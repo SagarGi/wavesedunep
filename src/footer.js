4a4b3f915f163bd8996742c03f7874cb8b396303
SagarGi <sagargurung1001@gmail.com>
2020-04-11 17:55:53 +0545

    footer

--- src/footer.js
+++ src/footer.js
@@ -5,8 +5,24 @@ const Footer = () =>
 {
     return(
         <div className = "main-footer">
-            <div class = "footer">
+            <div class = "container text-white">
 
+                <div className = "row">
+
+                    <div className = "col-lg-6">
+
+                      <p className = "copy">© 2020 Waves Education Foundation all right reserved.</p>
+
+                    </div>
+
+                     <div className = "col-lg-6">
+
+                      <p className = "copy">Developed By : Sagar Gurung</p>
+
+                    </div>
+
+                </div>
+                   
             </div>
          
         </div>
