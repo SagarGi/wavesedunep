ffba33a2e6a18631a8eb033a4fc83f9ee8327761
SagarGi <sagargurung1001@gmail.com>
2020-04-20 13:43:24 +0545

    form

--- src/FreeCounselling.js
+++ src/FreeCounselling.js
@@ -10,28 +10,24 @@ class FreeCounselling extends Component
         return(
             <div>
                  <img className = "contactusimage" src = { require('./images/freecounselling.jpg')}></img>
-                 <div className = "container">
-                     <h1 className = "mt-5">Free Counselling</h1>
-                </div>
+
+
+                 <div className = "container heading text-center">
+                    <h1 className = "freecounsell">Free Counselling From</h1>
+                 </div>
                 <div className = 'container formdiv'>
-
+              
                 <form action="/action_page.php">
-                    <label for="fname">First Name</label>
+                    <label for="fname">Name</label>
                     <input type="text" id="fname" name="firstname" placeholder="Your name.." />
-
-                    <label for="lname">Last Name</label>
-                    <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
-
                     <label for="email">Email</label>
                     <input type="email" id="email" name="email" placeholder="Your last name.." />
-
-                    <label for="country">Country</label>
-                    <select id="country" name="country">
-                    <option value="australia">Australia</option>
-                    <option value="canada">Canada</option>
-                    <option value="usa">USA</option>
-                    </select>
-                
+                    <label for="number">Mobile Number</label>
+                    <input type="text" id="number" name="number" placeholder="Your Number.." />
+                    <label for="subject">Subject</label>
+                    <input type="text" id="subject" name="subject" placeholder="subject.." />
+                    <label for="subject">Comment</label><br></br>
+                    <textarea id="comment" name="comment"></textarea>
                     <input type="submit" value="Submit" />
                 </form>
 
