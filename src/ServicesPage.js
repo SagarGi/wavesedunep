31df45e080fb2b60925bb6ebd733e78ce19e49aa
SagarGi <sagargurung1001@gmail.com>
2020-04-23 10:18:51 +0545

    language

--- src/ServicesPage.js
+++ src/ServicesPage.js
@@ -11,7 +11,7 @@ class ServicesPage extends Component
             <div>
                <img className = "contactusimage" src = { require('./images/ourservices.jpg')}></img>
                  <div className = "container">
-                     <h1 className = "mt-5 font-weight-bold">1. STUDY ABROAD</h1>
+                     <h1 className = "mt-5 font-weight-bold">STUDY ABROAD</h1>
                      <hr className = "servicesheading"></hr>
 
                      <div className = "container">
@@ -80,7 +80,7 @@ class ServicesPage extends Component
 
                      {/* Test Preparation */}
 
-                     <h1 className = "mt-5 font-weight-bold">2. TEST PREPARATION</h1>
+                     <h1 className = "mt-5 font-weight-bold">TEST PREPARATION</h1>
                      <hr className = "servicesheading"></hr>
                     <img className = "ielts" src = { require('./images/ielts.jpg')}></img>
 
@@ -178,6 +178,124 @@ class ServicesPage extends Component
 
                     </div>
 
+
+                    {/* japanese language */}
+                    <h1 className = "mt-5 font-weight-bold">JAPANESE LANGUAGE</h1>
+                     <hr className = "servicesheading"></hr>
+                     <img className = "japanese" src = { require('./images/japanese.jpg')}></img>
+
+                    <div className = "container">
+
+                        <div className = "row">
+
+                            <div className = "col-lg-2">
+
+                            </div>
+
+                            <div className = "col-lg-8">
+                            
+                            <p>The JLPT has five levels: N1, N2, N3, N4 and N5. The easiest level is N5 and the most difficult level is N1.</p>
+                            <p>N4 and N5 measure the level of understanding of basic Japanese mainly learned in class. N1and N2 measure the level of understanding of Japanese used in a broad range of scenes 
+                            in actual everyday life. N3 is a bridging level between N1/N2 and N4/N5.</p>
+
+                            <p>Linguistic competence required for the JLPT is expressed in terms of language activities, such as Reading and Listening, as shown in the table below. Wh
+                            t noted in the table, Language Knowledge, such as Vocabulary and Grammar, is also required for successful execution of these activities.</p>
+                                <div className = "row">
+
+                                    <div className = 'col-lg-6'>
+
+                                    <div class="card">
+                                        <div class="card-header bg-primary text-white">N1</div>
+                                        <div class="card-body">
+                                        <h4>Reading</h4>
+                                        One is able to read writings with logical complexity and/or abstract writings on a variety of topics, such as newspaper
+                                        editorials and critiques, and comprehend both their structures and contents. <br></br> <br></br>
+                                        <h4>Writing</h4>
+                                        <p>One is able to comprehend orally presented materials such as coherent conversations, news reports, and lectures, 
+                                        spoken at natural speed in a broad variety of settings, and is able to follow their ideas and comprehend their
+                                         contents comprehensively. One is also able to understand the details of the presented materials such as the 
+                                         relationships among the people involved, the logical structures, and the essential points.</p>
+                                    </div>  
+                                    </div>
+
+                                    </div>
+
+                                    <div className = 'col-lg-6'>
+
+                                    <div class="card">
+                                        <div class="card-header bg-primary text-white">N2</div>
+                                        <div class="card-body">
+
+                                        <h4>Reading</h4>
+                                        One is able to read materials written clearly on a variety of topics, such as articles and commentaries in newspapers and magazines as well as simple critiques, and comprehend their contents.
+                                        ・One is also able to read written materials on general topics and follow their narratives as well as understand the intent of the writers.<br></br> <br></br>
+                                        <h4>Writing</h4>
+                                        <p>One is able to comprehend orally presented materials such as coherent conversations and news reports, spoken at nearly natural speed in everyday situations as well as in a variety of settings,
+                                         and is able to follow their ideas and comprehend their contents. One is also able to understand the relationships among the people 
+                                         involved and the essential points of the presented materials.</p>
+                                       
+                                    </div>  
+                                    </div>
+
+                                    </div>
+
+                                    <div className = 'col-lg-6'>
+
+                                    <div class="card">
+                                        <div class="card-header bg-primary text-white">N3</div>
+                                        <div class="card-body">
+                                        <h4>Reading</h4>
+                                        One is able to read and understand written materials with specific contents concerning everyday topics.
+                                        ・One is also able to grasp summary information such as newspaper headlines.
+                                        ・In addition, one is also able to read slightly difficult writings encountered in everyday situations and understand the main points of the content if some alternative phrases are available to aid one’s understanding.<br></br> <br></br>
+                                        <h4>Writing</h4>
+                                        <p>One is able to listen and comprehend coherent conversations in everyday situations, spoken at near-natural speed, and is generally able to follow their contents as well as grasp the relationships among the people involved.</p>
+                                    </div>  
+                                    </div>
+
+                                    </div>
+
+                                    <div className = 'col-lg-6'>
+
+                                    <div class="card">
+                                        <div class="card-header bg-primary text-white">N4</div>
+                                        <div class="card-body">
+                                        <h4>Reading</h4>
+                                        One is able to read and understand passages on familiar daily topics written in basic vocabulary and kanji.<br></br> <br></br>
+                                        <h4>Writing</h4>
+                                        <p>One is able to listen and comprehend conversations encountered in daily life and generally follow their contents, provided that they are spoken slowly.</p>
+                                    </div>  
+                                    </div>
+
+                                    </div>
+
+                                    <div className = 'col-lg-6'>
+
+                                    <div class="card">
+                                        <div class="card-header bg-primary text-white">N5</div>
+                                        <div class="card-body">
+                                        <h4>Reading</h4>
+                                        One is able to read and understand typical expressions and sentences written in hiragana, katakana, and basic kanji.<br></br> <br></br>
+                                        <h4>Writing</h4>
+                                        <p>One is able to listen and comprehend conversations about topics regularly encountered in daily life and classroom situations, and is able to pick up necessary information from short conversations spoken slowly.</p>
+                                    </div>  
+                                    </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div className = "col-lg-2">
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    
                 </div>
                 <Secondfooter />
                 <Footer />
